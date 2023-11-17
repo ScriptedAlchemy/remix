16a0ea537a4d92b3328cd673adca94cb9123b4b1
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-11-17 15:51:02 -0800

    test esm cjs

--- playground/build.mjs
+++ playground/build.mjs
@@ -1,22 +1,27 @@
 import { createRsbuild } from '@rsbuild/core';
 import config from './rsbuild.config.mjs';
 import * as fs from 'fs';
+import * as path from 'path';
 import packageJSon from './package.json' assert { type: 'json' };
 import { readConfig } from '@remix-run/dev/dist/config.js';
+
 const start = async () => {
   const remixConfig = await readConfig();
-
   const client = await createRsbuild({
     target: ['web'],
-    rsbuildConfig: config,
-  })
+    rsbuildConfig: config
+  });
   const server = await createRsbuild({
     target: ['node'],
-    rsbuildConfig: config,
-  })
+    rsbuildConfig: config
+  });
   await client.build();
   await server.build();
-  const sr = await import('./build/index.mjs')
+  if(remixConfig.serverModuleFormat === 'esm') {
+    const sr = await import('./build/index.mjs');
+  } else {
+    const sr = await import('./build/index.js');
+  }
 };
 
 start();
