148455f5e384d3a1b38ba0096b995e0ea1f9ea86
ScriptedAlchemy <zackaryjackson@bytedance.com>
2023-12-06 00:44:20 -0800

    improve devserver

--- playground/server.mjs
+++ playground/server.mjs
@@ -10,6 +10,10 @@ app.use(express.static("public"));
 app.use('/server', express.static("build"));
 app.all("*", createRequestHandler({ build }));
 
-app.listen(3000, () => {
-  console.log(`Server started at http://localhost:3000`);
-});
+
+export const serve = ()=>{
+  app.listen(3000, () => {
+    console.log(`Server started at http://localhost:3000`);
+  });
+}
+export default app;
